e30b648e0babdaa89ac2c134cdf02b2a863e9832
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-03 01:15:45 +0000

    imported the needed assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { useState } from 'react'
+import DesktopImage from './assets/hero-desktop.jpg'
+import MobileImage from './assets/hero-mobile.jpg'
+import DesktopBgPattern from './assets/bg-pattern-desktop.svg'
+import logo from './assets/logo.svg'
+import {MdError} from 'react-icons/md'
+import {MdKeyboardArrowRight} from 'react-icons/md'
 
 function App() {
+  const [screenWidth,setScreenWidth]=useState(window.innerWidth);
 
   return (
    <div className="bg-red-300">
